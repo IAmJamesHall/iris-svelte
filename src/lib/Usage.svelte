8f71c165fc27d16a4630539c018761fa5c1ac741
James Hall <hi@jameshall.xyz>
2023-04-08 06:14:00 +0000

    Reverted Usage component

--- src/lib/Usage.svelte
+++ src/lib/Usage.svelte
@@ -1,16 +1,6 @@
 <script>
     import { usage } from '$lib/util/stores';
 
-    export let api = "openAI";
-
-    // @ts-ignore
-    console.log(api);
-    const apiStats = $usage[api]
-    console.log($usage);
 </script>
 
-{#if api == "openAI" || api == "kagi"}
-<div id="usage">Tokens: {apiStats.tokens}. Cost: ${Number(apiStats.cost).toFixed(6)}</div>
-{:else}
-<div id="usage">Invalid API type</div>
-{/if}
+<div id="usage">Tokens: {$usage.tokens}. Cost: ${Number($usage.cost).toFixed(6)}</div>
