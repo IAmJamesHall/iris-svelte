61fe10b4a3dc9c33eb41aad6d9097361802a39e8
James Hall <hi@jameshall.xyz>
2023-04-21 07:47:55 +0000

    Bulma overhaul of the chat/settings/apikeys pages

--- src/routes/apikeys/+page.svelte
+++ src/routes/apikeys/+page.svelte
@@ -20,17 +20,27 @@
     
 </script>
 
-<h1>API Keys</h1>
+<h1 class="subtitle">API Keys</h1>
 {#if !apiKeys.openAI }
-<h3>Enter your OpenAI API Key to use Iris</h3>
+<div class="notification is-warning">
+<h3 class="subtitle is-5">Enter your OpenAI API Key to use Iris</h3>
 <p>If you don't have one already, you can acquire one at <a href="https://platform.openai.com/account/api-keys">this link</a>. Your API key is never sent anywhere except in requests to OpenAI's API, and is only stored in your browser's local storage.</p>
+</div>
 {/if}
+<div class="field">
 <label for="openAI">OpenAI API Key</label>
-<input type="text" id="openAI" bind:value={apiKeys.openAI}>
+<div class="control">
+<input class="input" type="text" id="openAI" bind:value={apiKeys.openAI}>
+</div>
+</div>
 
 
+<div class="field">
 <label for="kagi">Kagi API Key</label>
-<input type="text" id="kagi" bind:value={apiKeys.kagi}>
+<div class="control">
+<input class="input" type="text" id="kagi" bind:value={apiKeys.kagi}>
+</div>
+</div>
 
-<button on:click={save} class="primary">Save</button>
+<button on:click={save} class="button is-primary">Save</button>
 <LocalStorage />
